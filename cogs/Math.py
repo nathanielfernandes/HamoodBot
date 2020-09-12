d95e99f5d3c1f2d510aec02fe0d2e029e8418435
Nathaniel Fernandes <nathaniel.s.fernandes@gmail.com>
2020-09-12 02:31:39 -0400

    added solve and code command

--- cogs/Math.py
+++ cogs/Math.py
@@ -1,62 +1,39 @@
 import discord
 from discord.ext import commands
 
+from modules.math_functions import base_conversion, solve_eq, run_code
+
 
 class Math(commands.Cog):
     """Quick Mafs"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-    @commands.command(aliases=["+"])
-    async def add(self, ctx, left: int, right: int):
-        """``add [number1] [number2]`` adds two numbers together"""
-        await ctx.send(f"**{str(left + right)}**")
-
-    @commands.command(aliases=["*"])
-    async def multiply(self, ctx, left: int, right: int):
-        """``multiply [number1] [number2]`` multiplies two numbers together"""
-        await ctx.send(f"**{str(left * right)}**")
-
-    @commands.command(aliases=["-"])
-    async def subtract(self, ctx, left: int, right: int):
-        """``subtract [number1] [number2]`` subtracts two numbers together"""
-        await ctx.send(f"**{str(left - right)}**")
-
-    @commands.command(aliases=["/"])
-    async def divide(self, ctx, left: int, right: int):
-        """``divide [number1] [number2]`` divides two numbers together"""
-        await ctx.send(f"**{str(left / right)}**")
 
     @commands.command()
     async def base(self, ctx, *, content: commands.clean_content):
         """``base [number)base] [base]`` converts numbers between bases"""
-        number, base2 = content.split(", ")
-        number, base1 = number.split(")")
-
         try:
-            number, base1, base2 = str(number).upper(), int(base1), int(base2)
-            if not 1 < base1 < 37 and not 1 < base2 < 37:
-                return
+            number, base2 = content.split(", ")
+            number, base1 = number.split(")")
         except ValueError:
+            await ctx.send("Invalid Input!")
             return
 
-        for num in number:
-            if num not in self.chars[:base1]:
-                return
+        answer = base_conversion(number, base1, base2)
+        await ctx.send(f"**Base {base1}:** `{number}`\n**Base {base2}:** `{answer}`\n")
 
-        temp_number = 0
-        for i in range(len(number)):
-            temp_number += self.chars.index(number[::-1][i]) * base1 ** i
+    @commands.command()
+    async def solve(self, ctx, *, content: commands.clean_content):
+        """``solve [equation]`` solve simple math equations"""
+        await ctx.send(f"`{solve_eq(content)}`")
 
-        answer = ""
-        while temp_number >= 1:
-            answer = self.chars[temp_number % base2] + answer
-            temp_number = temp_number // base2
-
-        content = answer
-        await ctx.send(f"**Base {base1}:** `{number}`\n**Base {base2}:** `{content}`\n")
+    @commands.command()
+    @commands.is_owner()
+    async def py(self, ctx, *, content: commands.clean_content):
+        """``py [code]`` lets me run code and out put it to the chat"""
+        out = run_code(content.strip("`"))
+        await ctx.send(f"```py\n{out}```")
 
 
 def setup(bot):
---
+++ modules/math_functions.py
@@ -0,0 +1,80 @@
+import sys
+import io
+from math import sqrt
+from sympy import symbols, Eq, solve, parse_expr
+
+
+chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+symbl = "abcdefghijklmnopqrstuvwxyz"
+
+
+def base_conversion(number, base1, base2):
+    try:
+        number, base1, base2 = str(number).upper(), int(base1), int(base2)
+        if not 1 < base1 < 37 and not 1 < base2 < 37:
+            return "<Invalid Entry>"
+    except ValueError:
+        return "<Invalid Entry>"
+
+    for num in number:
+        if num not in chars[:base1]:
+            return "<Invalid Entry>"
+
+    temp_number = 0
+    for i in range(len(number)):
+        temp_number += chars.index(number[::-1][i]) * base1 ** i
+
+    answer = ""
+    while temp_number >= 1:
+        answer = chars[temp_number % base2] + answer
+        temp_number = temp_number // base2
+
+    return answer
+
+
+def solve_eq(equation):
+    equation = equation.replace("^", "**").lower()
+    count = 0
+    solved = []
+    for i in symbl:
+        if i in equation:
+            count += 1
+    try:
+        if count > 0:
+            sol = solve(Eq(parse_expr(equation), 0), dict=True)
+            try:
+                for solution in sol:
+                    for k in solution.keys():
+                        if count < 2:
+                            solution[k] = round(eval(str(solution[k])), 3)
+                        solved.append(f"{k} = {solution[k]}")
+            except NameError:
+                return "No Solution"
+        else:
+            solved = eval(str(parse_expr(equation)))
+    except Exception:
+        return "Invalid Input"
+
+    return solved
+
+
+def run_code(code):
+    if "__" in code:
+        return "Dangerous Request!"
+
+    codeOut = io.StringIO()
+    sys.stdout = codeOut
+
+    try:
+        exec(code, {})
+    except Exception as e:
+        sys.stdout = sys.__stdout__
+        codeOut.close()
+        return e
+
+    out = codeOut.getvalue()
+    sys.stdout = sys.__stdout__
+    codeOut.close()
+
+    return out
+
