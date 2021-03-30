13d7c184161554cd48c235276e4ef77d881618b9
Nathaniel Fernandes <nathaniel.s.fernandes@gmail.com>
2021-03-30 02:02:02 -0400

    readded quickembed

--- HamoodBot.py
+++ HamoodBot.py
@@ -16,7 +16,7 @@ from discord.ext import commands, tasks
 from modules.image_functions import randomFile
 from utils.mongo import *
 from utils.market import Market
-from utils.helpers import pretty_time_delta
+from utils.helpers import pretty_time_delta, quick_embed
 from utils.s3 import S3
 
 if __name__ == "__main__":
@@ -61,6 +61,7 @@ if __name__ == "__main__":
     bot.market = Market(bot)
     bot.pretty_time_delta = pretty_time_delta
     bot.games = {}
+    bot.quick_embed = quick_embed
 
     @bot.event
     async def on_ready():
